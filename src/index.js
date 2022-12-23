e7d3f642633d3de3e713f5fdb8fce02f869f8d46
Gaurank Chaurasiya <gaurankchaurasiya1111@gmail.com>
2022-12-23 13:05:36 +0530

    new

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//How react Runs using Array
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+var row= [];
+var child= [];
+var box1= [];
+var box2= [];
+
+for(let k=0;k<8;k++)
+{
+	if(k%2===0)
+	{
+ 		box1.push(<div className="box-even"> </div>);
+	}
+	else
+	{
+		box1.push(<div className="box-odd"></div>)
+  	}
+
+}
+
+for(let k=0;k<8;k++){
+	if(k%2===0){
+ box2.push(<div className="box-odd"></div>)
+}else{
+	box2.push(<div className="box-even"></div>)
+  }
+
+}
+
+for(let j=0;j<2;j++){
+ child.push(<div className="row-child">{box1}{box2}</div>)
+}
+
+for(let i=0;i<2;i++){
+ row.push(<div className="row">{child}</div>)
+}
+
+
+ReactDOM.render(
+<React.Fragment>
+<h1>chess Board</h1>
+<div id="outer">
+  {row}
+</div>
+</React.Fragment>,
+document.getElementById('root'));
